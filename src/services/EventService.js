53633af017865dfb382bb546614816029e34400f
Vivian Hayes <vivian.xw7@gmail.com>
2023-11-19 15:58:36 -0600

    make API calls with axios

---
+++ src/services/EventService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/vivian-xw7/fake_server_1',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvents() {
+    return apiClient.get('/events') // gets the baseURL and adds /events at the end
+  }
+}
+
+
+
